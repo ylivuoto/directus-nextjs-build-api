cbe2c4871408b62f383fb23438efd95b0a4c2e3e
Ylivuoto <ylivuotokanava@gmail.com>
2023-08-18 14:07:59 +0300

    Auth with access_token

--- index.ts
+++ index.ts
@@ -1,21 +1,8 @@
-import { createDirectus, authentication, rest, refresh } from '@directus/sdk';
+import { createDirectus, authentication, rest, createItem } from '@directus/sdk';
 import express, { Express, Request, Response } from 'express';
 import { exec } from 'node:child_process';
 
 require('dotenv').config();
-
-const parseCookie: any = (str:string) => {
-    if (str == "")
-	return {};
-    
-  return str
-  .split(';')
-  .map(v => v.split('='))
-      .reduce((acc: any, v: any) => {
-      acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
-    return acc;
-  }, {});
-};
 
 const app: Express = express();
 const port: string | undefined = process.env.PORT || "3001";
@@ -29,14 +16,14 @@ app.post('/build', async (req: Request, res: Response) => {
 
     // Handling errors separately below
     try {
-	const cookie = req.headers?.cookie ? parseCookie(req.headers.cookie) : "";
-	const token = cookie?.directus_refresh_token ? cookie.directus_refresh_token : ""
+	const auth_token = req.headers?.authorization ? req.headers.authorization : "";
+	const access_token = auth_token.split(' ')[1]; // Token only
+	client.setToken(access_token)
 
-	if(!cookie) throw Error('Cookie not found from headers.')
-	if(!token) throw Error('Parsed no directus_refresh_token from cookie.')
+	if(!access_token) throw Error('Authorization bearer not found from headers.')
 	
 	// Throws exception if something wrong with credentials below
-	await client.request(refresh(token, 'cookie'));
+	await client.request(createItem('build_log', {info: `Deploy request from ${req.headers.host}.`}));
 
 	exec('npm run build', {
 	    cwd: cwd
@@ -44,11 +31,12 @@ app.post('/build', async (req: Request, res: Response) => {
 	    console.log('ERROR: ', err);
 	    console.log('STDOUT: ', stdout);
 	    console.log('STDERR: ', stderr);
+	    
+	    // As execution continues to here, lets send OK response
+	    res.sendStatus(200);
 	});
 	
-	// As execution continues to here, lets send OK response
-	res.sendStatus(200);
-	
+	client.setToken("");
     } catch(error) {
 	console.log(error)
 	res.sendStatus(403);
