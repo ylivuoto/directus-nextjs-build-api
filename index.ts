b4f4924ccaedd482b7f64fd7b709757070d74aff
Ylivuoto <ylivuotokanava@gmail.com>
2023-08-18 13:00:34 +0300

    Auth with refreshtoken, but logs current user off

--- index.ts
+++ index.ts
@@ -1,8 +1,21 @@
-import { createDirectus, authentication, rest, login } from '@directus/sdk';
+import { createDirectus, authentication, rest, refresh } from '@directus/sdk';
 import express, { Express, Request, Response } from 'express';
 import { exec } from 'node:child_process';
 
 require('dotenv').config();
+
+const parseCookie: any = (str:string) => {
+    if (str == "")
+	return {};
+    
+  return str
+  .split(';')
+  .map(v => v.split('='))
+      .reduce((acc: any, v: any) => {
+      acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+    return acc;
+  }, {});
+};
 
 const app: Express = express();
 const port: string | undefined = process.env.PORT || "3001";
@@ -13,24 +26,31 @@ const cwd: string | undefined = process.env.CWD || "/app"
 const client = createDirectus(host).with(authentication()).with(rest());
 
 app.post('/build', async (req: Request, res: Response) => {
-    const user = typeof req.headers?.user === 'string' ? req.headers.user : "";
-    const password = typeof req.headers?.password === 'string' ? req.headers.password : "";
 
-    // login using the authentication composable
+    // Handling errors separately below
     try {
-	// Throw exception if something wrong with credentials
-	await client.login(user, password, {});
+	const cookie = req.headers?.cookie ? parseCookie(req.headers.cookie) : "";
+	const token = cookie?.directus_refresh_token ? cookie.directus_refresh_token : ""
+
+	if(!cookie) throw Error('Cookie not found from headers.')
+	if(!token) throw Error('Parsed no directus_refresh_token from cookie.')
+	
+	// Throws exception if something wrong with credentials below
+	await client.request(refresh(token, 'cookie'));
+
 	exec('npm run build', {
 	    cwd: cwd
 	}, (err, stdout, stderr) => {
-	    console.log('ERROR: ', err, '\nSTDOUT: ', stdout, '\nSTDERR: ', stderr);
+	    console.log('ERROR: ', err);
+	    console.log('STDOUT: ', stdout);
+	    console.log('STDERR: ', stderr);
 	});
 	
 	// As execution continues to here, lets send OK response
 	res.sendStatus(200);
 	
     } catch(error) {
-	console.log((error as Error).message);
+	console.log(error)
 	res.sendStatus(403);
     }
 });
